7ae42603c3f1f1219c1ed50bb87fb6236bd721d4
Yue Fu <yue.fu@ucdconnect.ie>
2024-07-09 12:58:13 +0000

    Deleted views.py

--- backend/app01/views.py
+++
@@ -1,9 +0,0 @@
-from app01.models import Business
-from rest_framework import generics
-from app01.serializers import BusinessSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
-
-class RegisterBusinessView(generics.CreateAPIView):
-    queryset = Business.objects.all()
-    serializer_class = BusinessSerializer
-    permission_classes = [AllowAny]
