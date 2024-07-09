054c8aa4ec83fd67692ec3cc29d87cf39a5b099e
Yue Fu <yue.fu@ucdconnect.ie>
2024-07-09 13:05:11 +0000

    Added businessUnit.py to /backend/views

---
+++ backend/views/businessUnit.py
@@ -0,0 +1,14 @@
+from rest_framework.viewsets import ModelViewSet
+from app01.models import BusinessUnit
+from app01.serializer import BusinessUnitSerializer
+from app01.views.auth import MyAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+# from app01.views.authentication import CustomJWTAuthentication
+class BusinessUnitView(ModelViewSet):
+    authentication_classes = [JWTAuthentication]
+
+    permission_classes = [IsAuthenticated]
+    queryset = BusinessUnit.objects.all()
+    serializer_class = BusinessUnitSerializer
+
