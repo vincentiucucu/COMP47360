37dd7a22b62fbec757758c359b57a34e0822a56f
Yue Fu <yue.fu@ucdconnect.ie>
2024-07-11 12:19:45 +0000

    Updated serializers.py

--- backend/app01/serializers.py
+++ backend/app01/serializers.py
@@ -1,4 +1,4 @@
-from app01.models import Business
+from app01.models import Business,BusinessUnit,Vendor,Service,Log
 from rest_framework import serializers
 
 class BusinessSerializer(serializers.ModelSerializer):
@@ -12,3 +12,32 @@ class BusinessSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         return Business.objects.create_user(**validated_data)
+
+
+
+class BusinessUnitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BusinessUnit
+        fields = ["unit_name","permit_id","permit_expiry_date","unit_type"]
+
+
+
+
+class VendorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vendor
+        fields = ['vendor_name','licence_id','licence_expiry_date','vendor_email','vendor_phone_number' ]
+
+
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = ['unit','service_date','service_start_time','service_end_time','location_coords','location_address']
+
+
+
+class LogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Log
+        fields = ['operation','entity','entity_id','description','timestamp']
