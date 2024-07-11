ab13f264b9048dc2482ba391f33e89b158c7a7e3
Yue Fu <yue.fu@ucdconnect.ie>
2024-07-11 12:20:31 +0000

    Updated urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,11 +18,21 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from app01.views import RegisterBusinessView
+from rest_framework.routers import DefaultRouter
+from app01.view import businessUnit, log, service, vendor
+
+router = DefaultRouter()
+router.register(prefix="businessUnit", viewset=businessUnit.BusinessUnitView, basename='businessUnit')
+router.register(prefix="vendor", viewset=vendor.VendorView, basename='vendor')
+router.register(prefix="service", viewset=service.ServiceView, basename='service')
+router.register(prefix='log', viewset=log.LogView, basename='log')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("signup/", RegisterBusinessView.as_view(), name="signup"),
     path("login/", TokenObtainPairView.as_view(), name="get_token"),
     path("login/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
-    path("api-auth", include("rest_framework.urls"))
+    path("api-auth", include("rest_framework.urls")),
+
+    path("", include(router.urls)),
 ]
