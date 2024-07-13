bd415efbfaf37e2e6a5583fa36b9a84d688564ff
Yue Fu <yue.fu@ucdconnect.ie>
2024-07-13 20:52:45 +0000

    Updated serializers.py

--- backend/app01/serializers.py
+++ backend/app01/serializers.py
@@ -1,4 +1,4 @@
-from app01.models import Business,BusinessUnit,Vendor,Service,Log
+from app01.models import Business,BusinessUnit,Vendor,Service,Log,ServiceVendor
 from rest_framework import serializers
 
 class BusinessSerializer(serializers.ModelSerializer):
@@ -15,10 +15,12 @@ class BusinessSerializer(serializers.ModelSerializer):
 
 
 
+
+
 class BusinessUnitSerializer(serializers.ModelSerializer):
     class Meta:
         model = BusinessUnit
-        fields = ["unit_name","permit_id","permit_expiry_date","unit_type"]
+        fields = [ 'business', 'unit_name', 'permit_id', 'permit_expiry_date', 'unit_type']
 
 
 
@@ -26,18 +28,23 @@ class BusinessUnitSerializer(serializers.ModelSerializer):
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vendor
-        fields = ['vendor_name','licence_id','licence_expiry_date','vendor_email','vendor_phone_number' ]
+        fields = ['business','vendor_name','licence_id','licence_expiry_date','vendor_email','vendor_phone_number' ]
 
 
 
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
-        fields = ['unit','service_date','service_start_time','service_end_time','location_coords','location_address']
+        fields = ['business','unit','service_date','service_start_time','service_end_time','location_coords','location_address']
 
 
 
 class LogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Log
-        fields = ['operation','entity','entity_id','description','timestamp']
+        fields = ['business','operation','entity','entity_id','description','timestamp']
+
+class ServiceVendorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceVendor
+        fields = ['service','vendor','assigned_at']
