dcb3ce79c0904a573b9f25be0ce869a2e196351c
Yue Fu <yue.fu@ucdconnect.ie>
2024-07-13 21:15:52 +0000

    Updated serializers.py

--- backend/app01/serializers.py
+++ backend/app01/serializers.py
@@ -1,5 +1,6 @@
 from app01.models import Business,BusinessUnit,Vendor,Service,Log,ServiceVendor
 from rest_framework import serializers
+from django.contrib.gis.geos import Point
 
 class BusinessSerializer(serializers.ModelSerializer):
     
@@ -36,7 +37,18 @@ class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
         fields = ['business','unit','service_date','service_start_time','service_end_time','location_coords','location_address']
-
+    
+    def to_internal_value(self, data):
+        location_coords = data.get('location_coords')
+        if location_coords:
+            try:
+                lat, lng = map(float, location_coords.split(',')[:2])
+                data['location_coords'] = Point(lng, lat)
+            except (ValueError, TypeError):
+                raise serializers.ValidationError({
+                    'location_coords': 'Invalid format for location_coords. It should be "lat,lng".'
+                })
+        return super().to_internal_value(data)    
 
 
 class LogSerializer(serializers.ModelSerializer):
