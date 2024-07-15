b19ac371611f605ede9910f9f6642f3bb5c8bc72
Yue Fu <yue.fu@ucdconnect.ie>
2024-07-15 17:10:20 +0000

    Updated urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from app01.views import RegisterBusinessView, get_busyness_scores, get_recommendations_view
+from rest_framework.routers import DefaultRouter
+from app01.view import businessUnit, log, service, vendor,serviceVendor
+from rest_framework.documentation import include_docs_urls
+from rest_framework.permissions import AllowAny
 
+router = DefaultRouter()
+router.register(prefix="businessUnit", viewset=businessUnit.BusinessUnitView, basename='businessUnit')
+router.register(prefix="vendor", viewset=vendor.VendorView, basename='vendor')
+router.register(prefix="service", viewset=service.ServiceView, basename='service')
+router.register(prefix='log', viewset=log.LogView, basename='log')
+router.register(prefix='serviceVendor',viewset=serviceVendor.ServiceVendorView,basename='serviceVendor')
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("signup/", RegisterBusinessView.as_view(), name="signup"),
@@ -10,5 +20,9 @@ urlpatterns = [
     path("login/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
     path("api-auth", include("rest_framework.urls")),
     path('get_busyness_scores/', get_busyness_scores, name='get_busyness_scores'),
-    path('get_recommendations/', get_recommendations_view, name='get_recommendations')
+    path('get_recommendations/', get_recommendations_view, name='get_recommendations'),
+
+    path('docs/', include_docs_urls(title='Drf api', description='xxx',permission_classes=[AllowAny])),
+    path("api/", include(router.urls)),
+]
 ]
