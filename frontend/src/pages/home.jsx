e1d66fd3537d2cf4709af97a8c1bd43fabe348e9
Joel Sajan <joel.ukrasajan@ucdconnect.ie>
2024-07-24 12:08:23 +0100

    Minor_UI_Chages_For_PageNotFound_And_HomePage

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -35,198 +35,203 @@ const Main = () => {
   return (
     <Box
       sx={{
-        display: "flex",
-        justifyContent: "start",
         backgroundColor: "#333333",
-        color: "white",
-        height: "100%",
-        width: "100vw",
-        p: "0px",
-        ml: "-10px",
-        mr: "-10px",
-        mb: "-10px",
+        position: "absolute",
+        top: "0px",
+        left: "0px",
+        p:'20px'
       }}
     >
-      <Grid container spacing={2} sx={{ pb: "10px", mb: "10px", pt:'10px' }}>
-        <Grid item xs={6} sx={{ textAlign: "start" }}>
-          <Box sx={{ display: "flex", textAlign: "center" }}>
-            <img src={logo} alt="Logo" style={{ height: "30px" }} />
-          </Box>
-        </Grid>
-        <Grid item xs={6} sx={{ textAlign: "end" }}>
-          <Link
-            href="/login"
-            color="inherit"
-            sx={{ textDecoration: "none", marginRight: "20px" }}
-          >
-            Sign in →
-          </Link>
-        </Grid>
-        <Grid item xs={12} sx={{ textAlign: "center" }}>
-          <Typography
-            variant="h2"
-            sx={{
-              fontWeight: "bold",
-              fontSize: {
-                xs: "2rem",
-                sm: "3rem",
-                md: "4rem",
-              },
-            }}
-          >
-            Mobile Food Vending Made Easy
-          </Typography>
-          <Typography
-            variant="h5"
-            sx={{
-              marginTop: "10px",
-              marginBottom: "20px",
-              fontSize: {
-                xs: "1rem",
-                sm: "1.25rem",
-                md: "1.5rem",
-              },
-            }}
-          >
-            A Data-Driven Approach to Managing Your Mobile Food Vending Business
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Grid
-            item
-            xs={12}
-            sx={buttonStyle}
-            onClick={() => handleButton("Dashboard")}
-          >
+      <Box
+        sx={{
+          backgroundColor: "#333333",
+          color: "white",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <Grid container spacing={2} sx={{}}>
+          <Grid item xs={6} sx={{ textAlign: "start" }}>
+            <Box sx={{ display: "flex", textAlign: "center" }}>
+              <img src={logo} alt="Logo" style={{ height: "30px" }} />
+            </Box>
+          </Grid>
+          <Grid item xs={6} sx={{ textAlign: "end" }}>
+            <Link
+              href="/login"
+              color="inherit"
+              sx={{ textDecoration: "none", marginRight: "20px" }}
+            >
+              Sign in →
+            </Link>
+          </Grid>
+          <Grid item xs={12} sx={{ textAlign: "center" }}>
+            <Typography
+              variant="h2"
+              sx={{
+                fontWeight: "bold",
+                fontSize: {
+                  xs: "2rem",
+                  sm: "3rem",
+                  md: "4rem",
+                },
+              }}
+            >
+              Mobile Food Vending Made Easy
+            </Typography>
             <Typography
               variant="h5"
               sx={{
-                fontWeight: "bold",
-                marginBottom: "10px",
-                fontSize: {
-                  xs: "1.2rem",
-                  sm: "1.5rem",
-                  md: "1.75rem",
-                },
-              }}
-            >
-              High-level Dashboard
-            </Typography>
-            <Typography
-              variant="body1"
-              sx={{
+                marginTop: "10px",
                 marginBottom: "20px",
                 fontSize: {
-                  xs: "0.875rem",
-                  sm: "1rem",
-                  md: "1.125rem",
+                  xs: "1rem",
+                  sm: "1.25rem",
+                  md: "1.5rem",
                 },
               }}
             >
-              Track key metrics and monitor your business' growth
+              A Data-Driven Approach to Managing Your Mobile Food Vending
+              Business
             </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Grid
+              item
+              xs={12}
+              sx={buttonStyle}
+              onClick={() => handleButton("Dashboard")}
+            >
+              <Typography
+                variant="h5"
+                sx={{
+                  fontWeight: "bold",
+                  marginBottom: "10px",
+                  fontSize: {
+                    xs: "1.2rem",
+                    sm: "1.5rem",
+                    md: "1.75rem",
+                  },
+                }}
+              >
+                High-level Dashboard
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  marginBottom: "20px",
+                  fontSize: {
+                    xs: "0.875rem",
+                    sm: "1rem",
+                    md: "1.125rem",
+                  },
+                }}
+              >
+                Track key metrics and monitor your business' growth
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sx={buttonStyle}
+              onClick={() => handleButton("Business")}
+            >
+              <Typography
+                variant="h5"
+                sx={{
+                  fontWeight: "bold",
+                  marginBottom: "10px",
+                  fontSize: {
+                    xs: "1.2rem",
+                    sm: "1.5rem",
+                    md: "1.75rem",
+                  },
+                }}
+              >
+                Business Management
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  marginBottom: "20px",
+                  fontSize: {
+                    xs: "0.875rem",
+                    sm: "1rem",
+                    md: "1.125rem",
+                  },
+                }}
+              >
+                Add and monitor business units and authorized vendors
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sx={buttonStyle}
+              onClick={() => handleButton("Location")}
+            >
+              <Typography
+                variant="h5"
+                sx={{
+                  fontWeight: "bold",
+                  marginBottom: "10px",
+                  fontSize: {
+                    xs: "1.2rem",
+                    sm: "1.5rem",
+                    md: "1.75rem",
+                  },
+                }}
+              >
+                Location Recommendation
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  marginBottom: "20px",
+                  fontSize: {
+                    xs: "0.875rem",
+                    sm: "1rem",
+                    md: "1.125rem",
+                  },
+                }}
+              >
+                Plan your set-up location based on busyness data
+              </Typography>
+            </Grid>
           </Grid>
           <Grid
             item
             xs={12}
-            sx={buttonStyle}
-            onClick={() => handleButton("Business")}
+            md={8}
+            sx={{ textAlign: "center", marginTop: "0px" }}
           >
-            <Typography
-              variant="h5"
-              sx={{
-                fontWeight: "bold",
-                marginBottom: "10px",
-                fontSize: {
-                  xs: "1.2rem",
-                  sm: "1.5rem",
-                  md: "1.75rem",
-                },
+            <img
+              src={image}
+              alt="Overview"
+              style={{
+                width: "100%",
+                maxWidth: "550px",
+                justifyContent: "start",
               }}
-            >
-              Business Management
-            </Typography>
-            <Typography
-              variant="body1"
-              sx={{
-                marginBottom: "20px",
-                fontSize: {
-                  xs: "0.875rem",
-                  sm: "1rem",
-                  md: "1.125rem",
-                },
-              }}
-            >
-              Add and monitor business units and authorized vendors
-            </Typography>
+            />
           </Grid>
-          <Grid
-            item
-            xs={12}
-            sx={buttonStyle}
-            onClick={() => handleButton("Location")}
-          >
-            <Typography
-              variant="h5"
+          <Grid item xs={12} sx={{ textAlign: "center", marginTop: "40px" }}>
+            <Button
+              variant="contained"
               sx={{
-                fontWeight: "bold",
-                marginBottom: "10px",
-                fontSize: {
-                  xs: "1.2rem",
-                  sm: "1.5rem",
-                  md: "1.75rem",
-                },
+                backgroundColor: "orangered",
+                color: "white",
+                padding: "10px 20px",
+                fontSize: "18px",
+                "&:hover": { backgroundColor: "darkorange" },
               }}
+              href="/login"
             >
-              Location Recommendation
-            </Typography>
-            <Typography
-              variant="body1"
-              sx={{
-                marginBottom: "20px",
-                fontSize: {
-                  xs: "0.875rem",
-                  sm: "1rem",
-                  md: "1.125rem",
-                },
-              }}
-            >
-              Plan your set-up location based on busyness data
-            </Typography>
+              Start Today - It's Free!
+            </Button>
           </Grid>
         </Grid>
-        <Grid
-          item
-          xs={12}
-          md={8}
-          sx={{ textAlign: "center", marginTop: "0px" }}
-        >
-          <img
-            src={image}
-            alt="Overview"
-            style={{
-              width: "100%",
-              maxWidth: "550px",
-              justifyContent: "start",
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sx={{ textAlign: "center", marginTop: "40px" }}>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: "orangered",
-              color: "white",
-              padding: "10px 20px",
-              fontSize: "18px",
-              "&:hover": { backgroundColor: "darkorange" },
-            }}
-            href="/login"
-          >
-            Start Today - It's Free!
-          </Button>
-        </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 };
